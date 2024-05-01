d29d74b30a750559ff8be26904309db9485520f6
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-01 13:16:23 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240501/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;
+    cin>>n;
+    int arr[2001]={0,};
+    for(int i=0;i<2*n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+2*n);
+    int max=0;
+    for(int i=0;i<n;i++)
+    {
+        if(max<arr[i]+arr[2*n-i]) max=arr[i]+arr[2*n-i];
+    }
+    cout<<max;
+    return 0;
+}
