d71ba877914ccf821fbb0e5ba804dbfced7f6f7e
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-01 16:17:27 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240501/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n=0;
+    cin>>n;
+    int arr[101]={0,};
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        if(i%2==0)
+        {
+            sort(arr,arr+i);
+            cout<<arr[i/2]<<' ';
+        }
+    }
+    return 0;
+}
