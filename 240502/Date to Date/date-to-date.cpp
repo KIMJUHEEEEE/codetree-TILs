4628a71fd2ad9e759e67d79d166de811e3d4f36a
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-02 12:03:19 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240502/Date to Date/date-to-date.cpp
+++ 240502/Date to Date/date-to-date.cpp
@@ -8,13 +8,13 @@ int main() {
     int date[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
     int sum=0;
     
-    sum+=date[m1]-d1;
+    sum+=date[m1]-d1+1;
     sum+=d2;
     for(int i=m1+1;i<m2;i++)
     {
         sum+=date[i];
     }
     if(m1==m2&&d1==d2) sum=1;
-    cout<<sum+1;
+    cout<<sum;
     return 0;
 }
