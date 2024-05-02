cd8f4b21f54390f135afdd8dcb2bbc145e8eb451
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-02 12:22:09 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240502/요일 맞추기/guess-day-of-week.cpp
+++ 240502/요일 맞추기/guess-day-of-week.cpp
@@ -4,32 +4,42 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int m1,m2,d1,d2;
-    string week[7]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
-    int date[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-    cin>>m1>>d1>>m2>>d2;
-    int sum=0;
-    if(m1==m2)
+    int m1, m2, d1, d2;
+    string week[7] = { "Mon","Tue","Wed","Thu","Fri","Sat","Sun" };
+    int date[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+    cin >> m1 >> d1 >> m2 >> d2;
+    int sum = 0;
+    if (m1 == m2)
     {
-        sum+=d2-d1;
+        sum += d2 - d1;
+    }
+    else if (m1 > m2)
+    {
+        sum += date[m2] - d2-1;
+        sum += d1-1;
+        for (int i = m2 + 1; i < m1; i++)
+        {
+            sum += date[i];
+        }
     }
     else
     {
-        sum+=date[m1]-d1;
-        sum+=d2;
-        for(int i=m1+1;i<m2;i++)
+        sum += date[m1] - d1;
+        sum += d2;
+        for (int i = m1 + 1; i < m2; i++)
         {
-            sum+=date[i];
+            sum += date[i];
         }
     }
     string ans;
-    if(sum<0)
+    if (sum < 0)
     {
-        sum*=(-1);
-        sum=7-sum%7;
-        ans=week[sum];
+        sum *= (-1);
+        sum = sum % 7;
+        sum = 7 - sum;
+        ans = week[sum];
     }
-    else ans=week[sum%7];
-    cout<<ans;
+    else ans = week[sum % 7];
+    cout << ans;
     return 0;
 }
