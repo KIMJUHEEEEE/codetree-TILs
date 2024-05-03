e82b00b9ecc622158c593985c7d1522f5028336b
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-03 14:09:45 +0900

    Title: 흰검 칠하기, Time: 17ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240503/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int white[2001]={0,};
+    int black[2001]={0,};
+    char now[2001]={0,};
+    int n=0;
+    cin>>n;
+    int dx=1000;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        char b;
+        cin>>a>>b;
+        if(b=='R')
+        {
+            for(int j=0;j<a;j++)
+            {
+                white[dx+j]++;
+                now[dx+j]='b';
+            }
+            dx=dx+a-1;
+        }
+        else
+        {
+            for(int j=0;j<a;j++)
+            {
+                black[dx-j]++;
+                now[dx-j]='w';
+            }
+            dx=dx-a+1;
+        }
+    }
+    int gray=0;
+    int bb=0;
+    int ww=0;
+    for(int i=0;i<2001;i++)
+    {
+        if(white[i]>=2&&black[i]>=2)
+        {
+            gray++;
+            now[i]='g';
+        }
+        else
+        {
+            if(now[i]=='b') bb++;
+            else if(now[i]=='w') ww++;
+        }
+    }
+    cout<<ww<<' '<<bb<<' '<<gray;
+    return 0;
+}
