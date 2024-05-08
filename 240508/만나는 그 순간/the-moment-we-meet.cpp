7389b2d1885b6b21b09e984995d25fffe58d8819
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-08 10:28:28 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240508/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+int arra[1001];
+int arrb[1001];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    int da = 0;
+    int db = 0;
+    int sa = 0;
+    int sb = 0;
+    for (int i = 0; i < n; i++)
+    {
+        char c;
+        int s;
+        cin >> c >> s;
+        if (c == 'R')
+        {
+            for (int j = 0; j < s; j++)
+            {
+                da++;
+                sa++;
+                arra[sa] = da;
+            }
+        }
+        else if (c == 'L')
+        {
+            for (int j = 0; j < s; j++)
+            {
+                da--;
+                sa++;
+                arra[sa] = da;
+            }
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        char c;
+        int s;
+        cin >> c >> s;
+        if (c == 'R')
+        {
+            for (int j = 0; j < s; j++)
+            {
+                db++;
+                sb++;
+                arrb[sb] = db;
+            }
+        }
+        else if (c == 'L')
+        {
+            for (int j = 0; j < s; j++)
+            {
+                db--;
+                sb++;
+                arrb[sb] = db;
+            }
+        }
+    }
+    for (int i = 1; i < 1001; i++)
+    {
+        if (arra[i] == arrb[i])
+        {
+            cout << i; break;
+        }
+    }
+    return 0;
+}
