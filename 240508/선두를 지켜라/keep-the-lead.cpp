b3d44f527ad7358bb5ccb8df3054669203f84a38
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-08 11:03:41 +0900

    Title: 선두를 지켜라, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240508/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int arra[1001];
+int arrb[1001];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin>>n>>m;
+    int da=0;
+    int db=0;
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        for(int j=0;j<b;j++)
+        {
+            arra[da]=arra[da-1]+a;
+            da++;
+        }
+    }
+    for(int i=0;i<m;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        for(int j=0;j<b;j++)
+        {
+            arrb[db]=arrb[db-1]+a;
+            db++;
+        }
+    }
+    int cnt=0;
+    char ck='0';
+    for(int i=1;i<1001;i++)
+    {
+        if(arra[i]>arrb[i])
+        {
+            if(ck=='0') ck='a';
+            if(ck!='a')
+            {
+                cnt++;
+                ck='a';
+            }
+        }
+        else if(arrb[i]>arra[i])
+        {
+            if(ck=='0') ck='b';
+            if(ck!='b')
+            {
+                cnt++;
+                ck='b';
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
