1c740daebde530c57054d2741213115240ba6373
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-10 17:25:07 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240510/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[101]={0,};
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int cnt=0;
+    for(int i=0;i<n-2;i++)
+    {
+        for(int j=i+1;j<n-1;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                if(arr[i]<=arr[j]&&arr[j]<=arr[k])
+                {
+                    cnt++;
+                }
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
