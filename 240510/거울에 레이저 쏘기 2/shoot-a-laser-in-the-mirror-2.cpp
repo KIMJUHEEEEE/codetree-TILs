100601e6426393645c6a079b54d7cf3c8ab507f5
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-10 16:13:39 +0900

    Title: 거울에 레이저 쏘기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240510/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240510/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -47,11 +47,12 @@ int main() {
             {
                 x = i; y = j;
                 check = 1;
+                break;
             }
         }
         if (check == 1) break;
     }
-    dir = (k - 1) / 4;//남서북동
+    dir = (k - 1) / n;//남서북동
     int cnt = 0;
     x += dx[dir];
     y += dy[dir];
@@ -59,19 +60,24 @@ int main() {
     {
         if (arr[x][y] != '/' && arr[x][y] != '\\')
         {
-            cnt++;
             break;
         }
         else if (arr[x][y] == '/')
         {
-            dir = (dir + 1) % 4;
+            if (dir == 0) dir = 1;
+            else if (dir == 1) dir = 0;
+            else if (dir == 2) dir = 3;
+            else dir = 2;
             x += dx[dir];
             y += dy[dir];
             cnt++;
         }
         else if (arr[x][y] == '\\')
         {
-            dir = (dir + 3) % 4;
+            if (dir == 0) dir = 3;
+            else if (dir == 1) dir = 2;
+            else if (dir == 2) dir = 1;
+            else dir = 0;
             x += dx[dir];
             y += dy[dir];
             cnt++;
