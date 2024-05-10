5157559bfd70fcef716810b70df78ed69695d3e0
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-10 15:50:58 +0900

    Title: 거울에 레이저 쏘기 2, Time: 14ms, Memory: 2MB, Status: Wrong Answer - CodeTree

---
+++ 240510/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,-1,0,1 };//남서북동
+using namespace std;
+char arr[1002][1002];
+int arrd[1002][1002];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    int x = 0;
+    int y = 1;
+    int dir = 3;
+    for (int i = 1; i <= 4 * n; i++)
+    {
+        if (x == 0 && y == 0 || x == n + 1 && y == 0 || x == n + 1 && y == n + 1 || x == 0 && y == n + 1)
+        {
+
+
+            dir = (dir + 1) % 4;
+            x += dx[dir];
+            y += dy[dir];
+            i--;
+
+        }
+        else {
+            arrd[x][y] = i;
+            x += dx[dir];
+            y += dy[dir];
+        }
+    }
+    int k;
+    cin >> k;
+    bool check = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arrd[i][j] == k)
+            {
+                x = i; y = j;
+                check = 1;
+            }
+        }
+        if (check == 1) break;
+    }
+    dir = (k - 1) / 4;//남서북동
+    int cnt = 0;
+    x += dx[dir];
+    y += dy[dir];
+    while (1)
+    {
+        if (arr[x][y] != '/' && arr[x][y] != '\\')
+        {
+            cnt++;
+            break;
+        }
+        else if (arr[x][y] == '/')
+        {
+            dir = (dir + 1) % 4;
+            x += dx[dir];
+            y += dy[dir];
+            cnt++;
+        }
+        else if (arr[x][y] == '\\')
+        {
+            dir = (dir + 3) % 4;
+            x += dx[dir];
+            y += dy[dir];
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
