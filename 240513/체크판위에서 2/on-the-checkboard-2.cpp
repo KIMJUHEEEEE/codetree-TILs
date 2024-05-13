4610eb4529f6d8e673811390fe7c1d6ce8c3013b
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-13 09:51:20 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240513/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[16][16]={0,};
+    int r,c;
+    cin>>r>>c;
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int x=0;
+    int y=0;
+    char st;
+    int cnt=0;
+    for(int i=x+1;i<r-2;i++)
+    {
+        st=arr[0][0];
+        for(int j=y+1;j<c-2;j++)
+        {
+            if(st!=arr[i][j])
+            {
+                st=arr[i][j];
+                for(int a=i+1;a<r-1;a++)
+                {
+                    for(int b=j+1;b<c-1;b++)
+                    {
+                        if(st!=arr[a][b]&&arr[a][b]!=arr[r-1][c-1])
+                        {
+                            cnt++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
