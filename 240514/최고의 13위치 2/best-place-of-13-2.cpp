6065ef4634347ec0091d96ea9cc2940a090f1f90
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-14 14:47:14 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240514/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240514/최고의 13위치 2/best-place-of-13-2.cpp
@@ -4,50 +4,56 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
-    int arr[21][21]={0,};
-    for(int i=0;i<n;i++)
+    cin >> n;
+    int arr[21][21] = { 0, };
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            cin>>arr[i][j];
+            cin >> arr[i][j];
         }
     }
-    int max=0;
-    int maxb=0;
-    for(int i=0;i<n;i++)
+    int max = 0;
+   
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
-        {   bool arrb[21][21]={0,};
-            int cnt=0;
-            for(int k=0;k<3;k++)
+        for (int j = 0; j < n-2; j++)
+        {
+            bool arrb[21][21] = { 0, };
+            int cnt = 0;
+            for (int k = 0; k < 3; k++)
             {
-                arrb[i][j+k]=1;
-                if(arr[i][j+k]==1&&j+k<n)
-                {cnt++; }
-            }
-            for(int a=0;a<n;a++)
-            {
-                for(int b=0;b<n;b++)
+                arrb[i][j + k] = 1;
+                if (arr[i][j + k] == 1 && j + k < n)
                 {
-                    int cntb=0;
-                    if(arrb[a][b]==0)
+                    cnt++;
+                }
+            }
+            //cout << "cnt"<<i<<' '<<j<<' '<<cnt << endl;
+            int maxb = 0;
+            for (int a = 0; a < n; a++)
+            {
+                for (int b = 0; b < n-2; b++)
+                {
+                    int cntb = 0;
+                    if (arrb[a][b] == 0)
                     {
-                        for(int k=0;k<3;k++)
+                        for (int k = 0; k < 3; k++)
                         {
-                            if(arr[a][b+k]==1&&b+k<n)
+                            if (arr[a][b + k] == 1 && b + k < n)
                             {
                                 cntb++;
                             }
                         }
                     }
-                    if(maxb<cntb) maxb=cntb;
+                    //cout <<"cntb"<< a<<' ' << b<<' ' << cntb << endl;
+                    if (maxb < cntb) maxb = cntb;
                 }
             }
-            arrb[21][21]={0,};
-            if(cnt>max) max=cnt+maxb;
+            arrb[21][21] = { 0, };
+            if (cnt+maxb > max) max = cnt + maxb;
         }
     }
-    cout<<max;
+    cout << max;
     return 0;
 }
