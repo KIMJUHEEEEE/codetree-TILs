06b4af6fc2f4e5d51f13b6cf5c69ceefefc6b535
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-15 23:55:31 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240515/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240515/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n,k;
     cin>>n>>k;
-    int arr[101]={0,};
+    int arr[501]={0,};
     int maxidx=0;
     for(int i=0;i<n;i++)
     {
@@ -15,7 +15,7 @@ int main() {
         if(maxidx<b) maxidx=b;
     }
     int max=0;
-    for(int i=k;i<=maxidx-k;i++)
+    for(int i=k;i<=maxidx;i++)
     {
         int sum=0;
         for(int j=-k;j<=k;j++)
