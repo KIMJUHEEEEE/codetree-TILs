9c039e1f2c5ed680439dc8c0be2ae371f99223c8
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-16 11:32:19 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240516/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
+++ 240516/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -4,23 +4,31 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
-    int a1,b1,c1,a2,b2,c2;
-    cin>>a1>>b1>>c1;
-    cin>>a2>>b2>>c2;
-    int cnt;    int mul=1;
-    if(n>5) {
-    cnt=250; n=5;
-    if(abs(a1-a2)<=n-1) mul*=abs(n-abs(a1-a2));
-    if(abs(b1-b2)<=n-1) mul*=abs(n-abs(b1-b2));
-    if(abs(c1-c2)<=n-1) mul*=abs(n-abs(c1-c2));}
+    cin >> n;
+    int a1, b1, c1, a2, b2, c2;
+    cin >> a1 >> b1 >> c1;
+    cin >> a2 >> b2 >> c2;
+    int cnt;    int mul = 1;
+    if (n > 5) {
+        cnt = 250; 
+        if (a1 >= n - 1) a1 -=n;
+        if (b1 >= n - 1) b1 -= n;
+        if (c1 >= n - 1) c1 -= n;
+        if (a2 >= n - 1) a2 -= n;
+        if (b2 >= n - 1) b2 -= n;
+        if (c2 >= n - 1) c2 -= n;
+        n = 5;
+        if (abs(a1 - a2) <= n - 1) mul *= abs(n - abs(a1 - a2));
+        if (abs(b1 - b2) <= n - 1) mul *= abs(n - abs(b1 - b2));
+        if (abs(c1 - c2) <= n - 1) mul *= abs(n - abs(c1 - c2));
+    }
     else
     {
-    cnt=n*n*n*2;
-    if(abs(a1-a2)<=n-1) mul*=n;
-    if(abs(b1-b2)<=n-1) mul*=n;
-    if(abs(c1-c2)<=n-1) mul*=n;
+        cnt = n * n * n * 2;
+        if (abs(a1 - a2) <= n - 1) mul *= n;
+        if (abs(b1 - b2) <= n - 1) mul *= n;
+        if (abs(c1 - c2) <= n - 1) mul *= n;
     }
-    cout<<cnt-mul;
+    cout << cnt - mul;
     return 0;
 }
