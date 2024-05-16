86d8927bbb0bad412fe53a3c7364a88a8a599d25
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-16 11:21:10 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240516/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
+++ 240516/두 가지로 열리는 자물쇠/a-two-way-lock.cpp
@@ -8,16 +8,19 @@ int main() {
     int a1,b1,c1,a2,b2,c2;
     cin>>a1>>b2>>c1;
     cin>>a2>>b2>>c2;
-    int cnt;
-    if(n>5) {cnt=250; n=5;}
-    else
-    {
-        cnt=n*n*n*2;
-    }
-    int mul=1;
+    int cnt;    int mul=1;
+    if(n>5) {
+    cnt=250; n=5;
     if(abs(a1-a2)<=n-1) mul*=(n-abs(a1-a2));
     if(abs(b1-b2)<=n-1) mul*=(n-abs(b1-b2));
-    if(abs(c1-c2)<=n-1) mul*=(n-abs(c1-c2));
+    if(abs(c1-c2)<=n-1) mul*=(n-abs(c1-c2));}
+    else
+    {
+    cnt=n*n*n*2;
+    if(abs(a1-a2)<=n-1) mul*=n;
+    if(abs(b1-b2)<=n-1) mul*=n;
+    if(abs(c1-c2)<=n-1) mul*=n;
+    }
     cout<<cnt-mul;
     return 0;
 }
