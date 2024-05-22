8e5b32cd04d784a3e1a45284e3a0829cc6878181
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-22 17:49:49 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240522/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 240522/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -2,36 +2,55 @@
 #include <algorithm>
 using namespace std;
 
+class present{
+    public:
+    int price;
+    int deli;
+};
+bool cmp(present a, present b)
+{
+    if(a.price==b.price)
+    {
+        return a.deli<b.deli;
+    }
+    return a.price<b.price;
+}
 int main() {
     // 여기에 코드를 작성해주세요.
     int n,b;
     cin>>n>>b;
-    int arr[1001][2]={0,};
+    present p[1001];
     for(int i=0;i<n;i++)
     {
-        cin>>arr[i][0]>>arr[i][1];
+        int a,b;
+        cin>>a>>b;
+        p[i].price=a;
+        p[i].deli=b;
     }
     int max=0;
     for(int i=0;i<n;i++)
     {
+        present tmp[1001];
+        for(int j=0;j<n;j++)
+        {
+            tmp[j]=p[j];
+        }
+        tmp[i].price/=2;
+        sort(tmp,tmp+n,cmp);
         int sum=0;
         int cnt=0;
         for(int j=0;j<n;j++)
         {
-            int tmpsum=sum+arr[j][0]+arr[j][1];
+            int tmpsum=sum+tmp[j].price+tmp[j].deli;
             cnt++;
-            if(i==j) {
-                tmpsum-=arr[j][0]/2;
-                if(tmpsum>b) {cnt--; break;}
-                                
-                continue;
-            }
-            
-            if(tmpsum>b) {
+            if(tmpsum>b)
+            {
                 cnt--;
+                break;
             }
-            else{
-                sum+=arr[j][0]+arr[j][1];
+            else
+            {
+                sum+=tmp[j].price+tmp[j].deli;
             }
         }
         if(max<cnt) max=cnt;
