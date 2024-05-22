e33ece8388b498d2cd4298e30a31607d41f28546
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-22 16:28:29 +0900

    Title: 개발자의 순위, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240522/개발자의 순위/developer's-rank.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int k,n;
+    int arr[11][21]={0,};
+    int price[11][21]={0,};
+    cin>>k>>n;
+    for(int i=0;i<k;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+            price[i][arr[i][j]]=j;
+        }
+    }
+    int ans=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            int cnt=0;
+            for(int l=0;l<k;l++)
+            {
+                if(price[l][i]<price[l][j]) cnt++;
+            }
+            if(cnt==k) ans++;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
