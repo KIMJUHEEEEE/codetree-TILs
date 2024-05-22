42b033b3fa4e41cd4af7574f15af8602072254cb
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-22 16:47:14 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240522/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,k;
+    cin>>n>>k;
+    int arr[101]={0,};
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int max=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                if(j-i<=k)
+                {
+                    if(max<j) max=arr[i];
+                }
+            }
+        }
+    }
+    cout<<max;
+    return 0;
+}
