028c351c7b8f68e0aefc4593c2e85f10bdcd9ddb
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-22 17:19:13 +0900

    Title: 스승의 은혜 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240522/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 240522/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -21,7 +21,7 @@ int main() {
             if(i==j) {
                 sum+=arr[i][0]/2+arr[i][1];
                 cnt++;
-                if(sum>b) cnt--;
+                if(sum>b) {cnt--; break;}
                 continue;
             }
             int tmpsum=sum+arr[j][0]+arr[j][1];
