6e4f0f46fa7a151d8ffb7e1639a9b934285cb044
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-22 17:32:13 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240522/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 240522/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -6,47 +6,32 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n,b;
     cin>>n>>b;
-    int arra[1001]={0,};
-    int arrb[1001]={0,};
+    int arr[1001][2]={0,};
     for(int i=0;i<n;i++)
     {
-        cin>>arra[i]>>arrb[i];
+        cin>>arr[i][0]>>arr[i][1];
     }
     int max=0;
-    for(int i=0;i<n-1;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        {
-            if(arra[i]>arra[j])
-            {
-                int tmp=arra[i];
-                arra[i]=arra[j];
-                arra[j]=tmp;
-                tmp=arrb[i];
-                arrb[i]=arrb[j];
-                arrb[j]=tmp;
-            }
-        }
-    }
     for(int i=0;i<n;i++)
     {
         int sum=0;
         int cnt=0;
         for(int j=0;j<n;j++)
         {
+            int tmpsum=sum+arr[j][0]+arr[j][1];
+            cnt++;
             if(i==j) {
-                sum+=arra[i]/2+arrb[i];
-                cnt++;
-                if(sum>b) {cnt--; break;}
+                tmpsum-=arr[j][0]/2;
+                if(tmpsum>b) {cnt--; break;}
+                                
                 continue;
             }
-            int tmpsum=sum+arra[j]+arrb[j];
-            cnt++;
+            
             if(tmpsum>b) {
                 cnt--;
             }
             else{
-                sum+=arra[j]+arrb[j];
+                sum+=arr[j][0]+arr[j][1];
             }
         }
         if(max<cnt) max=cnt;
