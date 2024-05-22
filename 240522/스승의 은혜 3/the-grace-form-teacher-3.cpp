3db96a1c99cb1986067f35daba1a5daa35e08fdd
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-22 17:12:14 +0900

    Title: 스승의 은혜 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240522/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,b;
+    cin>>n>>b;
+    int arr[1001][2]={0,};
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i][0]>>arr[i][1];
+    }
+    int max=0;
+    for(int i=0;i<n;i++)
+    {
+        int sum=0;
+        int cnt=0;
+        for(int j=0;j<n;j++)
+        {
+            if(i==j) {
+                sum+=arr[i][0]/2+arr[i][1];
+                cnt++;
+                continue;
+            }
+            int tmpsum=sum+arr[j][0]+arr[j][1];
+            cnt++;
+            if(tmpsum>b) {
+                cnt--;
+            }
+            else{
+                sum+=arr[j][0]+arr[j][1];
+            }
+        }
+        if(max<cnt) max=cnt;
+    }
+    cout<<max;
+    return 0;
+}
