9199f9c6a6c3c146354a8e8a021c9605b6f6b400
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-23 09:31:59 +0900

    Title: 빙산의 일각 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240523/빙산의 일각 2/the-tip-of-the-iceberg-2.cpp
+++ 240523/빙산의 일각 2/the-tip-of-the-iceberg-2.cpp
@@ -7,26 +7,30 @@ int main() {
     cin>>n;
     int arr[101]={0,};
     int max=0;
+    int min=9999;
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
         if(arr[i]>max) max=arr[i];
+        if(arr[i]<min) min=arr[i];
     }
     int ans=0;
-    for(int i=1;i<max;i++)
+    for(int i=min;i<max;i++)
     {
         int tmp[101]={0,};
         int cnt=0;
         for(int j=0;j<n;j++)
         {
             tmp[j]=arr[j]-i;
+            if(tmp[j]<=0) tmp[j]=0;
             
         }
         for(int j=0;j<n;j++)
         {
-            if(tmp[j]<=0) tmp[j]=0;
+
             if(tmp[j]>0&&tmp[j+1]==0) 
             {
+                //cout<<i<<' '<<j<<endl;
                 cnt++;
             }
         }
