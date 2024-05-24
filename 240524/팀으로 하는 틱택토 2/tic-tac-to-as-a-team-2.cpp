57fb76b4600d48437ae8e6c8a2d6868598a62ae4
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-24 11:46:46 +0900

    Title: 팀으로 하는 틱택토 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240524/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
+++ 240524/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -12,6 +12,8 @@ int main() {
         }
     }
     int ans = 0;
+    int ck[9][2] = { 0, };
+
     for (int i = 0; i < 3; i++)
     {
         int cnt[10] = { 0, };
@@ -20,25 +22,66 @@ int main() {
             cnt[arr[i][j]-48]++;
         }
         int num = 0;
+        int a = 0;
+        int b = 0;
         for (int j = 0; j < 10; j++)
         {
-            if (cnt[j] > 0) num++;
+            if (cnt[j] > 0)
+            {
+                num++;
+                if (a == 0) a = j;
+                else b = j;
+            }
         }
-        if (num == 2) ans++;
-    }
+        if (num == 2)
+        {
+            bool check = 0;
+            for (int j = 0; j < 9; j++)
+            {
+                if (ck[j][0] == a && ck[j][1] == b) check = 1;
+            }
+            if (check == 0)
+            {
+                ans++;
+                ck[ans][0] = a;
+                ck[ans][1] = b;
+            }
+        }
+    } 
     for (int i = 0; i < 3; i++)
     {
+
         int cnt[10] = { 0, };
         for (int j = 0; j < 3; j++)
         {
             cnt[arr[j][i]-48]++;
         }
         int num = 0;
+        int a = 0;
+        int b = 0;
         for (int j = 0; j < 10; j++)
         {
-            if (cnt[j] > 0) num++;
+            if (cnt[j] > 0)
+            {
+                num++;
+                if (a == 0) a = j;
+                else b = j;
+            }
         }
-        if (num == 2) ans++;
+        if (num == 2)
+        {
+            bool check = 0;
+            for (int j = 0; j < 9; j++)
+            {
+                if (ck[j][0] == a && ck[j][1] == b) check = 1;
+            }
+            if (check == 0)
+            {
+                ans++;
+                ck[ans][0] = a;
+                ck[ans][1] = b;
+            }
+        }
     }
     int cnt[10] = { 0, };
     int num = 0;
@@ -46,22 +89,62 @@ int main() {
     {
         cnt[arr[i][i]-48]++;
     }
+    int a = 0;
+    int b = 0;
     for (int j = 0; j < 10; j++)
     {
-        if (cnt[j] > 0) num++;
+        if (cnt[j] > 0)
+        {
+            num++;
+            if (a == 0) a = j;
+            else b = j;
+        }
     }
-    if (num == 2) ans++;
+    if (num == 2)
+    {
+        bool check = 0;
+        for (int j = 0; j < 9; j++)
+        {
+            if (ck[j][0] == a && ck[j][1] == b) check = 1;
+        }
+        if (check == 0)
+        {
+            ans++;
+            ck[ans][0] = a;
+            ck[ans][1] = b;
+        }
+    }
     int cnt2[10] = { 0, };
     num = 0;
     for (int i = 0; i < 3; i++)
     {
         cnt2[arr[i][2 - i]-48]++;
     }
+    a = 0;
+    b = 0;
     for (int j = 0; j < 10; j++)
     {
-        if (cnt2[j] > 0) num++;
+        if (cnt[j] > 0)
+        {
+            num++;
+            if (a == 0) a = j;
+            else b = j;
+        }
     }
-    if (num == 2) ans++;
+    if (num == 2)
+    {
+        bool check = 0;
+        for (int j = 0; j < 9; j++)
+        {
+            if (ck[j][0] == a && ck[j][1] == b) check = 1;
+        }
+        if (check == 0)
+        {
+            ans++;
+            ck[ans][0] = a;
+            ck[ans][1] = b;
+        }
+    }
     cout << ans;
     return 0;
 }
