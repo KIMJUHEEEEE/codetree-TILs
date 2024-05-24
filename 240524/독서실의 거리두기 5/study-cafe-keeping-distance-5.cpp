beb336d8a2a43f7fb6f6a2f04ce03f26bc11f8fc
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-24 16:33:31 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240524/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240524/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -5,31 +5,42 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
+    cin >> n;
     string arr;
-    cin>>arr;
-    int ans=0;
-    for(int i=0;i<n;i++)
+    cin >> arr;
+    int ans = 0;
+    string tmp;
+    for (int i = 0; i < n; i++)
     {
-        int max=9999;
-        if(arr[i]=='1') continue;
-        else
+        tmp = arr;
+        int min = 0;
+        if (tmp[i] == '0')
         {
-            for(int j=0;j<n;j++)
+            min = 999;
+            tmp[i] = '1';
+            for (int j = 0; j < n - 1; j++)
             {
-                int dis=0;
-                if(i==j) continue;
-                else{
-                    if(arr[j]=='1')
+
+                for (int k = j + 1; k < n; k++)
+                {
+                    int dis = 0;
+                    if (abs(k - j) > min) break;
+                    if (tmp[j] == '1' && tmp[k] == '1')
                     {
-                        dis=abs(j-i);
-            
-                if(max>dis) max=dis;
-                    }}
+                        dis = abs(k - j);
+                        if (dis < min) {
+                            min = dis; break;
+                        }
+                    }
+                }
             }
         }
-        if(ans<max) ans=max;
+        if (ans < min)
+        {
+            ans = min;
+        }
     }
-    cout<<ans;
+
+    cout << ans;
     return 0;
 }
