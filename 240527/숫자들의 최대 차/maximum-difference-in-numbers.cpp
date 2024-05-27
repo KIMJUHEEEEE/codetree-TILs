eae3d676161ab8b2644178866fb9d3b4a980f0ba
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-27 09:16:30 +0900

    Title: 숫자들의 최대 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240527/숫자들의 최대 차/maximum-difference-in-numbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,k;
+    cin>>n>>k;
+    int cnt[10001]={0,};
+    int max=0;
+    int min=99999;
+    for(int i=0;i<n;i++)
+    {
+        int a;
+        cin>>a;
+        cnt[a]++;
+        if(max<a) max=a;
+        if(min>a) min=a;
+    }
+    int ans=0;
+    for(int i=min;i<=max-k;i++)
+    {
+        int sum=0;
+        for(int j=i;j<=i+k;j++)
+        {
+            sum+=cnt[j];
+        }
+        if(sum>ans) ans=sum;
+    }
+    cout<<ans;
+
+    return 0;
+}
