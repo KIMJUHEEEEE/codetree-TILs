c3b4489cd07a97ba6eecad2e9cc22209d62fd64f
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-27 16:01:48 +0900

    Title: 언덕 깎기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240527/언덕 깎기/hill-cutting.cpp
+++ 240527/언덕 깎기/hill-cutting.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin>>n;
     int arr[1001]={0,};
+    int min=9999;
+    int max=0;
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
+        if(min>arr[i]) min=arr[i];
+        if(max<arr[i]) max=arr[i];
     }
     sort(arr,arr+n);
-    int cnt=1;
-    int ans=0;
-    while(1)
+    int ans=99999;
+    for(int i=min;i<=max-17;i++)
     {
-        arr[0]=arr[0]+1;
-        sort(arr,arr+n);
-        ans=cnt*cnt;
-        if(arr[n-1]-arr[0]<=17) {
-            
-            break;
+        int sum=0;
+        for(int k=0;k<n;k++)
+        {
+            if(arr[k]<i) sum+=(i-arr[k])*(i-arr[k]);
+            if(arr[k]>i+17) sum+=(arr[k]-i-17)*(arr[k]-i-17);
         }
-        arr[n-1]=arr[n-1]-1;
-        sort(arr,arr+n);
-        if(arr[n-1]-arr[0]<=17)
-            {
-                ans+=cnt*cnt;
-                break;
-            }
-        cnt++;
+
+        if(ans>sum) ans=sum;
     }
     cout<<ans;
     return 0;
