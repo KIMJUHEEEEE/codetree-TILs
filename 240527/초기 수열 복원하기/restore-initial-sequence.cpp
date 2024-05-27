1d47c5825ada77ba0c1624201adb5cbbd42b5def
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-27 10:42:40 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240527/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[1001]={0,};
+    int sum[1000]={0,};
+    for(int i=0;i<n-1;i++)
+    {
+        cin>>sum[i];
+    }
+   
+    for(int a=1;a<=n;a++)
+    {
+        int tmp[1001]={0,};
+        int tmpa=a;
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            tmp[i]=tmpa;
+            tmpa=sum[i]-tmp[i];
+            if(tmp[i]==tmpa) break;
+            cnt++;
+        }
+        if(cnt==n)
+        {
+            for(int i=0;i<n;i++)
+            {
+                cout<<tmp[i]<<' ';
+            }
+            break;
+        }
+    }
+    return 0;
+}
