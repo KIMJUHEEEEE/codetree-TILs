2026b4a9e019067e4e5cf2294402d46c0ff96a45
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-28 11:23:49 +0900

    Title: 최대 최소간의 차, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240528/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
+++ 240528/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
@@ -4,36 +4,44 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,k;
-    cin>>n>>k;
-    int arr[101]={0,};
-    for(int i=0;i<n;i++)
+    int n, k;
+    cin >> n >> k;
+    int arr[101] = { 0, };
+    int cnt[101] = { 0, };
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
+        cnt[arr[i]]++;
     }
-    int ans=0;
-    while(1)
+    int ans = 0;
+    while (1)
     {
-        sort(arr,arr+n);
-        int mini=arr[0];
-        int maxi=arr[n-1];
-        if(maxi-mini<=k)
+        sort(arr, arr + n);
+        if (arr[n-1] - arr[0] <= k)
         {
             break;
         }
         else
         {
-            arr[0]+=1;
-            ans++;
-            sort(arr,arr+n);
-            if(maxi-arr[0]<=k) break;
+            int maxi = cnt[arr[n - 1]];
+            int mini = cnt[arr[0]];
+            if (mini <= maxi)
+            {
+                for (int j = 0; j < mini; j++)
+                {
+                    arr[j] += 1; ans++;
+                }
+            }
             else
             {
-                arr[n-1]-=1;
-                ans++;
+                for (int j = 0; j < maxi; j++)
+                {
+                    arr[n - 1] -= 1; ans++;
+                }
             }
+
         }
     }
-    cout<<ans;
+    cout << ans;
     return 0;
 }
