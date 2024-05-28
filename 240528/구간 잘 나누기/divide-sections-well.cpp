6d4a9fff84fbbe9f19368abc8be4b43fdd66c258
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-28 11:05:43 +0900

    Title: 구간 잘 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240528/구간 잘 나누기/divide-sections-well.cpp
+++ 240528/구간 잘 나누기/divide-sections-well.cpp
@@ -1,54 +1,41 @@
 #include <iostream>
 using namespace std;
-int arr[102] = { 0, };
-int sum[102] = { 0, };
-int n;
-int mini=99999;
-
-void find(int pidx, int i, int cnt, int m,  int max)
-{
-    if (pidx > n||i>n) return;
-    if (cnt == m)
-    {
-       int num = sum[n] - sum[pidx] ;
-       if (num > max) max = num;
-       if (max < mini) mini = max;
-       return;
-    }
-    
-    for (int j = 0; j < n; j++)
-    {
-        int previdx = pidx;
-        int num = sum[i + j] - sum[previdx];
-        if (num > max) max = num;
-        previdx = i + j;
-        if (previdx > n||i+j>n) return;
-        find(previdx, i + j+1, cnt + 1, m, max);
-        previdx = pidx;
-    }
-    
-}
-
 
 int main() {
-    int m;
-    cin >> n >> m;
-
-    int arrmax = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> arr[i];
-        if (arrmax < arr[i]) arrmax = arr[i];
-        if (i == 0) sum[i] = arr[i];
-        else
-        {
-            sum[i] = arr[i] + sum[i - 1];
-        }
-    }
-    int ans = 99999999;
-    int i = 0;
-    find(i, i+1, 0, m - 1, 0);
-    cout << mini;
     // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin>>n>>m;
+    int arr[101]={0,};
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int ans=9999999;
+    for(int i=1;i<=1000;i++)
+    {
+        bool possible=true;
+        int num=1;
+        int cnt=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]>i) {
+                possible=false;
+                break;
+            }
+        
+        if(cnt+arr[j]>i)
+        {
+            cnt=0;
+            num++;
+        }
+        cnt+=arr[j];
+        }
+    if(possible&&num<=m)
+    {
+        ans=min(ans,i);
+    }
+    }
+    cout<<ans;
     return 0;
 }
