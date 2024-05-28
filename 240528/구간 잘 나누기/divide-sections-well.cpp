ef8c71e9e1768b982cc39ad7282278cc59d79207
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-28 10:54:31 +0900

    Title: 구간 잘 나누기, Time: 1097ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240528/구간 잘 나누기/divide-sections-well.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+int arr[102] = { 0, };
+int sum[102] = { 0, };
+int n;
+int mini=99999;
+
+void find(int pidx, int i, int cnt, int m,  int max)
+{
+    if (pidx > n||i>n) return;
+    if (cnt == m)
+    {
+       int num = sum[n] - sum[pidx] ;
+       if (num > max) max = num;
+       if (max < mini) mini = max;
+       return;
+    }
+    
+    for (int j = 0; j < n; j++)
+    {
+        int previdx = pidx;
+        int num = sum[i + j] - sum[previdx];
+        if (num > max) max = num;
+        previdx = i + j;
+        if (previdx > n) break;
+        find(previdx, i + j+1, cnt + 1, m, max);
+        previdx = pidx;
+    }
+    
+}
+
+
+int main() {
+    int m;
+    cin >> n >> m;
+
+    int arrmax = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+        if (arrmax < arr[i]) arrmax = arr[i];
+        if (i == 0) sum[i] = arr[i];
+        else
+        {
+            sum[i] = arr[i] + sum[i - 1];
+        }
+    }
+    int ans = 99999999;
+    int i = 0;
+    find(i, i+1, 0, m - 1, 0);
+    cout << mini;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
