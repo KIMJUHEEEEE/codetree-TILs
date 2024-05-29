545a572577b16b0118502a942b2c2b56c16e142f
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-29 11:15:46 +0900

    Title: 훌륭한 점프, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240529/훌륭한 점프/great-jump.cpp
+++ 240529/훌륭한 점프/great-jump.cpp
@@ -1,46 +1,44 @@
 #include <iostream>
 using namespace std;
 
-bool IsPossible(int arr[],int a,int n,int k)
+int IsPossible(int arr[], int a, int n, int k)
 {
-    int available_indices[101] = {0,};
+    int available_indices[101] = { 0, };
     int cnt = 0;
-
-    for(int i = 0; i < n; i++)
-        if(arr[i] >= a)
+    int maxi=0;
+    for (int i = 0; i < n; i++)
+        if (arr[i] <= a)
+        {
             available_indices[cnt++] = i;
-
-    for(int i = 1; i < cnt; i++) {
+            maxi = max(maxi, arr[i]);
+        }
+    if (cnt == 1) return 101;
+    for (int i = 1; i < cnt; i++) {
         int dist = available_indices[i] - available_indices[i - 1];
-        if(dist > k)
-            return false;
+        if (dist > k)
+            return 101;
     }
 
-    return true;
+    return maxi;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,k;
-    cin>>n>>k;
-    int arr[101]={0,};
-    int cnt[101]={0,};
-    int idx=0;
-    for(int i=0;i<n;i++)
+    int n, k;
+    cin >> n >> k;
+    int arr[101] = { 0, };
+    int maxnum = 0;
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
-        cnt[arr[i]]++;
-        if(idx<arr[i]) idx=arr[i];
+        cin >> arr[i];
+        maxnum=max(maxnum, arr[i]);
     }
-    int ans=0;
-    for(int i=1;i<101;i++)
+    int ans = 101;
+    for (int i = maxnum; i >=1;i--)
     {
-        if(cnt[i]==0) continue;
-        if(IsPossible(arr,i,n,k))
-        {
-            ans=max(ans,i);
-        }
+        int num=IsPossible(arr,i,n,k);
+        ans = min(ans,num );
     }
-    cout<<ans;
+    cout << ans;
     return 0;
 }
