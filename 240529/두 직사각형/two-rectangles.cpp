0974d602ff6e46ea9693fb56d26259e357730bbf
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-29 13:14:43 +0900

    Title: 두 직사각형, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240529/두 직사각형/two-rectangles.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x1,y1,x2,y2;
+    int a1,b1,a2,b2;
+    cin>>x1>>y1>>x2>>y2;
+    cin>>a1>>b1>>a2>>b2;
+    if(a1<=x2&&a1>=x1)
+    {
+        if((b1<=y2&&b1>=y1)||(b2>=y1&&b2<=y2)) cout<<"overlapping";
+        else cout<<"nonoverlapping";
+    }
+    else if(a2<=x2&&a2>=x1)
+    {
+        if((b1<=y2&&b1>=y1)||(b2>=y1&&b2<=y2)) cout<<"overlapping";
+        else cout<<"nonoverlapping";
+    }
+    else cout<<"nonoverlapping";
+    return 0;
+}
