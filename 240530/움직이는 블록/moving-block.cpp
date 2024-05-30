69faf130109d28780620f8e3e8ce80f43cc80286
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-30 10:20:52 +0900

    Title: 움직이는 블록, Time: 49ms, Memory: 0MB, Status: Passed - CodeTree

--- 240530/움직이는 블록/moving-block.cpp
+++ 240530/움직이는 블록/moving-block.cpp
@@ -4,43 +4,44 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-    cin>>n;
-    int arr[10001]={0,};
-    int sum=0;
-    for(int i=0;i<n;i++)
+    cin >> n;
+    int arr[10001] = { 0, };
+    int sum = 0;
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
-        sum+=arr[i];
+        cin >> arr[i];
+        sum += arr[i];
     }
-    int avg=sum/n;
-    int ans=0;
-    for(int i=0;i<n-1;i++)
+    int avg = sum / n;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i]>avg)
+        if (arr[i] > avg)
         {
-            for(int j=0;j<n;j++)
+            for (int j = 0; j < n; j++)
             {
-                if(i==j) continue;
-                if(arr[j]<avg)
+                if (i == j) continue;
+                if (arr[i] == avg) break;
+                if (arr[j] < avg)
                 {
-                    if(avg-arr[j]<=arr[i]-avg)
+                    if (avg - arr[j] <= arr[i] - avg)
                     {
-                        int need=avg-arr[j];
-                        arr[j]=avg;
-                        arr[i]=arr[i]-need;
-                        ans+=need;
+                        int need = avg - arr[j];
+                        arr[j] = avg;
+                        arr[i] = arr[i] - need;
+                        ans += need;
                     }
-                    else if(avg-arr[j]>arr[i]-avg)
+                    else if (avg - arr[j] > arr[i] - avg)
                     {
-                        int give=arr[i]-avg;
-                        arr[i]=avg;
-                        arr[j]+=give;
-                        ans+=give;
+                        int give = arr[i] - avg;
+                        arr[i] = avg;
+                        arr[j] += give;
+                        ans += give;
                     }
                 }
             }
         }
     }
-    cout<<ans;
+    cout << ans;
     return 0;
 }
