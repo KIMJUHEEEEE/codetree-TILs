f005f9aa14364d91557172c665a950fc01b506a0
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-05-30 10:16:06 +0900

    Title: 움직이는 블록, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240530/움직이는 블록/moving-block.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[10001]={0,};
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    int avg=sum/n;
+    int ans=0;
+    for(int i=0;i<n-1;i++)
+    {
+        if(arr[i]>avg)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j) continue;
+                if(arr[j]<avg)
+                {
+                    if(avg-arr[j]<=arr[i]-avg)
+                    {
+                        int need=avg-arr[j];
+                        arr[j]=avg;
+                        arr[i]=arr[i]-need;
+                        ans+=need;
+                    }
+                    else if(avg-arr[j]>arr[i]-avg)
+                    {
+                        int give=arr[i]-avg;
+                        arr[i]=avg;
+                        arr[j]+=give;
+                        ans+=give;
+                    }
+                }
+            }
+        }
+    }
+    cout<<ans;
+    return 0;
+}
