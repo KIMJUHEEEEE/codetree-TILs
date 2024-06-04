071986956730fe5b5a6fc3b4719137e56b621744
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-06-04 22:23:54 +0900

    Title: 황금비율 토스트, Time: 7ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240604/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin>>n>>m;
+    string str;
+    cin>>str;
+    list<char> l;
+    for(int i=0;i<n;i++)
+    {
+        l.push_back(str[i]);
+    }
+    list<char>::iterator it;
+    it=l.end();
+    for(int i=0;i<m;i++)
+    {
+        char c;
+        cin>>c;
+        if(c=='L'){
+            if(it!=l.begin())
+            {
+                it--;
+            }
+        }
+        else if(c=='R'){
+            if(it!=l.end())
+            {
+                it++;
+            }
+        }
+        else if(c=='P')
+        {
+            char a;
+            cin>>a;
+            l.insert(it,a);
+        }
+        else if(c=='D')
+        {
+            l.erase(it);
+
+        }
+    }
+    for(it =l.begin();it!=l.end();it++)
+    {
+        cout<<*it;
+    }
+    return 0;
+}
