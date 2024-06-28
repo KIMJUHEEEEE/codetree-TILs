29469f2c9677896266ef5f0820cb1ec2f2ea9309
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-06-28 16:24:39 +0900

    Title: 양수 직사각형의 최대 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240628/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m;
+    cin>>n>>m;
+    int arr[21][21]={0,};
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int ans=-1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            for(int y=0;y<n-i;y++)
+            {
+                for(int x=0;x<m-j;x++)
+                {
+                    int a=y;
+                    int b=x;
+                    bool check=1;
+                    for(int c=0;c<=a;c++)
+                    {
+                        for(int d=0;d<=b;d++)
+                        {
+                            if(arr[i+c][j+d]<0)
+                            {
+                                check=0;
+                                break;
+                            }
+                        }
+                        if(check==0) break;
+                    }
+                    if(check==1)
+                    {
+                        ans=max(ans,(a+1)*(b+1));
+                    }
+                }
+            }
+        }
+    }
+    cout<<ans;
+    return 0;
+}
