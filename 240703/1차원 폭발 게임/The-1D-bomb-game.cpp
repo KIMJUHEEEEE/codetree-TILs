a38fa3f556fd1296723cef952f245734427d0ebe
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-07-03 22:09:43 +0900

    Title: 1차원 폭발 게임, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240703/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240703/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -4,52 +4,57 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,m;
-    cin>>n>>m;
+    int n, m;
+    cin >> n >> m;
     vector<int> v;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         int a;
-        cin>>a;
+        cin >> a;
         v.push_back(a);
     }
-    int cnt=1;
-    int si=-1;
-    int ei=0;
-    if(m==1)
+    int cnt = 1;
+    int si = -1;
+    int ei = 0;
+    if (m == 1)
     {
-        cout<<0;
+        cout << 0;
         return 0;
     }
-    for(int i=0;i<v.size()-1;i++)
-    {       
-        if(v[i]==v[i+1])
-        {
-            cnt+=1;
-            if(si==-1) si=i;
-            ei=i+2;
-        }
-        else
-        {
-            if(cnt>=m)
-            {
-
-                v.erase(v.begin()+si,v.begin()+ei);
-                i=-1;
-            }
-            cnt=1;
-            si=-1;
-            ei=0;
-        }
-    }
-    if(cnt>=m)
-            {
-                v.erase(v.begin()+si,v.end());
-            }
-    cout<<v.size()<<endl;
-    for(int i=0;i<v.size();i++)
+    while (1)
     {
-        cout<<v[i]<<endl;
+        bool ck = 0;
+        for (int i = 0; i < v.size() - 1; i++)
+        {
+            if (v[i] == v[i + 1])
+            {
+                cnt += 1;
+                if (si == -1) si = i;
+                ei = i + 2;
+            }
+            else
+            {
+                if (cnt >= m)
+                {
+                    ck = 1;
+                    v.erase(v.begin() + si, v.begin() + ei);
+                    i = si - 1;
+                }
+                cnt = 1;
+                si = -1;
+                ei = 0;
+            }
+        }
+        if (ck == 0) break;
+    }
+    if (cnt >= m)
+    {
+        v.erase(v.begin() + si, v.begin()+ei);
+    }
+    cout << v.size() << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
     }
     return 0;
 }
