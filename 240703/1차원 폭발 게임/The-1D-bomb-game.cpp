b0839fe1af576e9967ce5ae221813026b2c9b373
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-07-03 22:04:42 +0900

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240703/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240703/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -14,34 +14,38 @@ int main() {
         v.push_back(a);
     }
     int cnt=1;
-    int si=0;
-    int ei=-1;
+    int si=-1;
+    int ei=0;
     if(m==1)
     {
         cout<<0;
         return 0;
     }
-    for(int i=v.size()-1;i>=0;i--)
+    for(int i=0;i<v.size()-1;i++)
     {       
-        if(v[i]==v[i-1])
+        if(v[i]==v[i+1])
         {
             cnt+=1;
-            si=i-1;
-            if(ei==-1) ei=i+1;
+            if(si==-1) si=i;
+            ei=i+2;
         }
         else
         {
             if(cnt>=m)
             {
+
                 v.erase(v.begin()+si,v.begin()+ei);
-                i=si+1;
+                i=-1;
             }
             cnt=1;
-            si=0;
-            ei=-1;
+            si=-1;
+            ei=0;
         }
     }
-
+    if(cnt>=m)
+            {
+                v.erase(v.begin()+si,v.end());
+            }
     cout<<v.size()<<endl;
     for(int i=0;i<v.size();i++)
     {
