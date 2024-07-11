83c5409a67696b05f74167f69513f8be953bfc87
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-07-11 09:38:18 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240711/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { -1,1,0,0 };
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, t;
+    cin >> n >> m >> t;
+    int arr[101][101] = { 0, };
+    int beads[101][101] = { 0, };
+    int cpbeads[101][101] = { 0, };
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = i = 0; i < m; i++)
+    {
+        int x, y;
+        cin >> y >> x;
+        beads[y - 1][x - 1] = 1;
+    }
+    for (int i = 0; i < t; i++)
+    {
+        for (int y = 0; y < n; y++)
+        {
+            for (int x = 0; x < n; x++)
+            {
+                if (beads[y][x] == 0) continue;
+                else
+                {
+                    int max = 0;
+                    int maxx = x;
+                    int maxy = y;
+                    for (int k = 0; k < 4; k++)
+                    {
+                        if (max < arr[y + dy[k]][x + dx[k]])
+                        {
+                            maxx = x + dx[k];
+                            maxy = y + dy[k];
+                            max = arr[maxy][maxx];
+                        }
+                    }
+
+                    beads[y][x] = 0;
+                    cpbeads[maxy][maxx]++;
+
+                }
+            }
+        }
+        for (int y = 0; y < n; y++)
+        {
+            for (int x = 0; x < n; x++)
+            {
+                beads[y][x] = cpbeads[y][x];
+            }
+        }
+        for (int y = 0; y < n; y++)
+        {
+            for (int x = 0; x < n; x++)
+            {
+                if (beads[y][x] > 1)
+                {
+                    beads[y][x] = 0;
+                }
+            }
+        }
+    }
+    int ans = 0;
+    for (int y = 0; y < n; y++)
+    {
+        for (int x = 0; x < n; x++)
+        {
+            if (beads[y][x] == 1) ans++;
+        }
+    }
+    cout << ans;
+    return 0;
+}
