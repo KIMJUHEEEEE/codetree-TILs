f35df075be21e8dcc808294eb477e36f6d0f107c
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-08-01 16:51:52 +0900

    Title: 외판원 순회, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+int n;
+int arr[11][11];
+bool ck[11];
+vector<int> v;
+int ans=INT_MAX;
+
+void rec(int idx)
+{
+    if(idx==n)
+    {
+        int sum=0;
+        for(int i=0;i<v.size()-1;i++)
+        {
+            sum+=arr[v[i]][v[i+1]];
+        }
+        sum+=arr[v[v.size()-1]][v[0]];
+        ans=min(ans,sum);
+        return;
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(ck[i]!=0) continue;
+        v.push_back(i);
+        ck[i]=1;
+        rec(idx+1);
+        v.pop_back();
+        ck[i]=0;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    rec(0);
+    cout<<ans;
+    return 0;
+}
