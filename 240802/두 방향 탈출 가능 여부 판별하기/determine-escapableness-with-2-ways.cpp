4801085badde2f9a4a8d9f04e5fad120e395661c
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-08-02 13:21:56 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240802/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m;
+int arr[101][101];
+int ans=0;
+
+bool ck(int x, int y)
+{
+    if(arr[y+1][x]==0&&arr[y][x+1]==0&&y+1>n||x+1>n) return false;
+    return true;
+}
+
+void dfs(int y, int x)
+{
+    if(ck(x,y)==false)
+    {
+        return ;
+    }
+    if(x==m-1&&y==n-1) {ans=1; return;}
+    if(arr[y+1][x]==1&&y+1<n)
+    {
+        dfs(y+1,x);
+    }
+    if(arr[y][x+1]==1&&x+1<n)
+    {
+        dfs(y,x+1);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    dfs(0,0);
+    cout<<ans;
+    return 0;
+}
