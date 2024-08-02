e11a527aa6433d4273f5a7619b939325bdb29963
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-08-02 13:11:59 +0900

    Title: 그래프 탐색, Time: 16ms, Memory: 5MB, Status: Passed - CodeTree

--- 240802/그래프 탐색/graph-traversal.cpp
+++ 240802/그래프 탐색/graph-traversal.cpp
@@ -27,6 +27,7 @@ void dfs(int idx, int cnt)
 
     for (int i = 0; i < n; i++)
     {
+        if (idx == i) continue;
         if (arr[idx][i] == 1 && visited[idx][i] == 0&&ck[i]==0)
         {
             visited[idx][i] = 1;
@@ -50,18 +51,7 @@ int main() {
         arr[a - 1][b - 1] = 1;
         arr[b - 1][a - 1] = 1;
     }
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i][0]==1)    
-        {
-            visited[i][0]=1;
-            visited[0][i]=1;
-            dfs(i, 1);
-            visited[i][0]=0;
-            visited[0][i]=0;
-        }
-    }
-
+    dfs(0, 0);
     for (int i = 1; i < n; i++)
     {
         if (ck[i] == 1) ans++;
