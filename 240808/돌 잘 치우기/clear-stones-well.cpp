0bb60cd388def4420dff7700b283d03014d5f25e
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-08-08 10:18:56 +0900

    Title: 돌 잘 치우기, Time: 649ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/돌 잘 치우기/clear-stones-well.cpp
+++ 240808/돌 잘 치우기/clear-stones-well.cpp
@@ -2,95 +2,100 @@
 #include <queue>
 #include <vector>
 using namespace std;
-int n,m,k;
+int n, m, k;
 int arr[101][101];
-int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
-bool visited[101][101];
-vector<pair<int,int>> stone;
-queue<pair<int,int>> q;
-vector<pair<int,int>> choice;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+vector<pair<int, int>> stone;
+queue<pair<int, int>> q;
+vector<pair<int, int>> choice;
+vector<pair<int,int>> start;
 bool ck[10];
 int ans;
 void dfs(int idx)
 {
-    if(idx==m)
+    if (idx == m)
     {
-        for(int i=0;i<m;i++)
+        bool visited[101][101]={0,};
+        for (int i = 0; i < m; i++)
         {
-            int a=choice[i].first;
-            int b=choice[i].second;
-            arr[a][b]=0;
+            int a = choice[i].first;
+            int b = choice[i].second;
+            arr[a][b] = 0;
         }
-        while(q.size()>0)
+        for(int i=0;i<start.size();i++)
         {
-            int y=q.front().first;
-            int x=q.front().second;
+            q.push(start[i]);
+        }
+        while (q.size() > 0)
+        {
+            int y = q.front().first;
+            int x = q.front().second;
             q.pop();
-            for(int j=0;j<4;j++)
+            for (int j = 0; j < 4; j++)
             {
-                if(y+dy[j]<0||x+dx[j]<0||y+dy[j]==n||x+dx[j]==n) continue;
-                if(arr[y+dy[j]][x+dx[j]]==0&&visited[y+dy[j]][x+dx[j]]==0)
+                if (y + dy[j] < 0 || x + dx[j] < 0 || y + dy[j] == n || x + dx[j] == n) continue;
+                if (arr[y + dy[j]][x + dx[j]] == 0 && visited[y + dy[j]][x + dx[j]] == 0)
                 {
-                    q.push(make_pair(y+dy[j],x+dx[j]));
-                    visited[y+dy[j]][x+dx[j]]=1;
+                    q.push(make_pair(y + dy[j], x + dx[j]));
+                    visited[y + dy[j]][x + dx[j]] = 1;
                 }
             }
         }
-        int sum=0;
-        for(int i=0;i<n;i++)
+        int sum = 0;
+        for (int i = 0; i < n; i++)
         {
-        for(int j=0;j<n;j++)
-        {
-            if(visited[i][j]==1)
+            for (int j = 0; j < n; j++)
             {
-                sum++;
+                if (visited[i][j] == 1)
+                {
+                    sum++;
+                }
             }
         }
-        }
-        ans=max(sum,ans);
-        for(int i=0;i<m;i++)
+        ans = max(sum, ans);
+        for (int i = 0; i < m; i++)
         {
-            int a=choice[i].first;
-            int b=choice[i].second;
-            arr[a][b]=1;
+            int a = choice[i].first;
+            int b = choice[i].second;
+            arr[a][b] = 1;
         }
         return;
     }
-    for(int i=0;i<stone.size();i++)
+    for (int i = 0; i < stone.size(); i++)
     {
-        if(ck[i]==0)
-        {choice.push_back(stone[i]);
-        ck[i]=1;
-        dfs(idx+1);
-        choice.pop_back();
-        ck[i]=0;
+        if (ck[i] == 0)
+        {
+            choice.push_back(stone[i]);
+            ck[i] = 1;
+            dfs(idx + 1);
+            choice.pop_back();
+            ck[i] = 0;
         }
     }
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    cin>>n>>k>>m;
-    for(int i=0;i<n;i++)
+    cin >> n >> k >> m;
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            cin>>arr[i][j];
-            if(arr[i][j]==1)
+            cin >> arr[i][j];
+            if (arr[i][j] == 1)
             {
-             stone.push_back(make_pair(i,j));   
+                stone.push_back(make_pair(i, j));
             }
         }
     }
-    for(int i=0;i<k;i++)
+    for (int i = 0; i < k; i++)
     {
-        int r,c;
-        cin>>r>>c;
-        q.push(make_pair(r-1,c-1));
-        visited[r-1][c-1]=1;
-        dfs(0);
+        int r, c;
+        cin >> r >> c;
+        start.push_back(make_pair(r-1,c-1));
     }
-    cout<<ans;
+    dfs(0);
+    cout << ans;
     return 0;
 }
