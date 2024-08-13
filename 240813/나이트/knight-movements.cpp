8b2be1e7e8ae504fa3bb89328bb2f1407623538e
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-08-13 09:20:03 +0900

    Title: 나이트, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240813/나이트/knight-movements.cpp
+++ 240813/나이트/knight-movements.cpp
@@ -12,6 +12,11 @@ int main() {
     cin>>n;
     int r1,c1,r2,c2;
     cin>>r1>>c1>>r2>>c2;
+    if(r1==r2&&c1==c2)
+    {
+        cout<<1;
+        return 0;
+    }
     visited[r1-1][c1-1]=1;
     queue<pair<int,int>> q;
     q.push(make_pair(r1-1,c1-1));
