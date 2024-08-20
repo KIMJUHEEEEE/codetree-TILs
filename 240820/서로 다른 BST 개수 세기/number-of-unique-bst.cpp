6f8858b41224cf1065d397f632538c74d48f5982
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-08-20 17:40:07 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
+++ 240820/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -8,15 +8,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
 
     cin>>n;
-
+    arr[0]=1;
     arr[1]=1;
     arr[2]=2;
     arr[3]=5;
     for(int i=4;i<=n;i++)
     {
-        for(int a=1;a<n;a++)
+        for(int a=0;a<n;a++)
         {
-            arr[i]+=arr[i-a]*arr[i-n+a];
+            arr[i]+=arr[i-a-1]*arr[a];
         }
     }
     cout<<arr[n];
