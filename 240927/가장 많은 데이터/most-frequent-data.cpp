caf892b9712925e166090ce2a639dcef6d8c99b8
KIMJUHEEEEE <48671595+KIMJUHEEEEE@users.noreply.github.com>
2024-09-27 14:10:35 +0900

    Title: 가장 많은 데이터, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240927/가장 많은 데이터/most-frequent-data.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    unordered_map<string,int> m;
+    int n;
+    cin>>n;
+    int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        string str;
+        cin>>str;
+        if(m.find(str)==m.end())
+        {
+            m.insert({str,1});
+        }
+        else
+        {
+            m[str]++;
+            ans=max(ans,m[str]);
+        }
+    }
+    cout<<ans;
+    return 0;
+}
